0be7bd99027a569fb81fd456f8606aa27a587bf8
weirdpattern <patricio@weirdpattern.com>
2017-07-10 16:04:41 -0500

    No install required

--- Install.ps1
+++
@@ -1,8 +0,0 @@
-#########################################################################
-# Copyright (c) 2017, Patricio Trevino                                  #
-# All rights reserved.                                                  #
-#                                                                       #
-# This source code is licensed under the MIT-style license found in the #
-# LICENSE file in the root directory of this source tree.               #
-#########################################################################
-
